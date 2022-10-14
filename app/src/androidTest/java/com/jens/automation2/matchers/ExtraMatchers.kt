62e0912f57dd3f4d30e995a86492ba635de49a80
Vladyslav_Shevchenk3 <Vladyslav_Shevchenko3>
2022-10-14 19:17:30 +0300

    Removed tests, which used Espresso

--- app/src/androidTest/java/com/jens/automation2/matchers/ExtraMatchers.kt
+++
@@ -1,5 +0,0 @@
-package com.jens.automation2.matchers
-
-fun actionItemWithName(itemName: String) = ActionItemTextMatcher(itemName)
-
-fun actionWithSeekBar(progress: Int) = ActionWithSeekBar(progress)
